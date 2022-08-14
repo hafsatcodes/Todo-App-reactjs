49ddc6dd67087c22407a8ad6b3eedd959d0c2027
hafsatcodes <65682252+hafsatcodes@users.noreply.github.com>
2022-08-14 10:48:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState("");
+  const [addTodo, setAddTodo] = useState([]);
+  // const [style, setStyle] = useState(false);
+  // const onCheck = () => {
+  //   setStyle("line-through");
+  // };
+
+  const onChange = (e) => {
+    setTodos(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todos === "") {
+      alert("Empty input");
+    } else {
+      setAddTodo((ls) => [...ls, todos]);
+      setTodos("");
+    }
+  };
+
+  const onClick = (todo) => {
+    setAddTodo((prev) => prev.filter((a) => a !== todo));
+  };
+  const onEdit = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1
+        style={{
+          color: "white",
+          fontSize: "60px",
+          margin: "20px",
+        }}
+      >
+        Todo List
+      </h1>
+      <div>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onSubmit={onSubmit}
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            type="text"
+            placeholder="Add todo..."
+            value={todos}
+            onChange={onChange}
+            className="form__field"
+          />
+          <button className="form-btn">Enter</button>
+        </form>
+        {addTodo.map((todo) => {
+          return (
+            <div key={todo} className="todo-box">
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <input
+                  className="check-box"
+                  type="checkbox"
+                  // onChange={onCheck}
+                />
+                <h1
+                  style={{
+                    fontSize: "40px",
+                    // textDecoration: style,
+                  }}
+                >
+                  {todo}
+                </h1>
+              </div>
+              <div>
+                <button className="btn active" onClick={() => onClick(todo)}>
+                  Remove
+                </button>
+                <button className="btn " onClick={onEdit}>
+                  Edit
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
